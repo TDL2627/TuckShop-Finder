7693a300ac2adb3e946e83c74b2cbf0861f834e3
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-26 09:53:44 +0200

    added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="foot">
+ <footer>
+      <p>TDL2627</p>
+  </footer>
+</div>
+ 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+footer{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    height: 50px;
+    width:0;
+    background: green;
+    color: white;
+}
+</style>
