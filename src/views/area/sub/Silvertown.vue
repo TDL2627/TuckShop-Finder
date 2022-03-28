6f0103c75f622329950d0cec9d1156faa07a5487
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-28 14:51:01 +0200

    added the first shop

--- src/views/area/sub/Silvertown.vue
+++ src/views/area/sub/Silvertown.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="sub-area">
 <h1>This is Silver Town</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 shop">
+           <h3>Ali's Shop</h3>
+           <p>Somalian</p>
+           <p>7AM- 10PM</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.0533859217135!2d18.528060417443847!3d-33.96546729999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc44acedc01b71%3A0x310865ff465d2da7!2s25%20Bongo%20Cres%2C%20Silvertown%2C%20Cape%20Town%2C%207764!5e0!3m2!1sen!2sza!4v1648471411513!5m2!1sen!2sza" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+         <img src="@/assets/shops/shop1.png" alt="shop">
+        </div>
+    </div>
+</div>
+
   </div>
 </template>
 
@@ -13,7 +26,11 @@ export default {
 <style scoped>
 .sub-area{
    padding-top:15%;
-  height: 70vh;
+  height: max-content;
   background: black;
+  padding-bottom: 15%;
+}
+.shop{
+    border: solid 2px red;
 }
 </style>
