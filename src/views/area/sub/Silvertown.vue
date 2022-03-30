19b23482b2fd084bc45a2dc715175ac3f4a714ce
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-30 08:51:06 +0200

    deployed to firebase

--- src/views/area/sub/Silvertown.vue
+++ src/views/area/sub/Silvertown.vue
@@ -2,25 +2,15 @@
   <div class="sub-area">
 <h1>This is Silver Town</h1>
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-4 shop">
-           <h3>Ali's Shop</h3>
-           <p>Somalian</p>
-           <p>7AM- 10PM</p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.0533859217135!2d18.528060417443847!3d-33.96546729999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc44acedc01b71%3A0x310865ff465d2da7!2s25%20Bongo%20Cres%2C%20Silvertown%2C%20Cape%20Town%2C%207764!5e0!3m2!1sen!2sza!4v1648471411513!5m2!1sen!2sza" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-         <img src="@/assets/shops/shop1.png" alt="shop">
-        </div>
-    </div>
-</div>
+
 
  <div class="container">
    <div class="row">
   <div class="col-lg-5  shop"  v-for="shop in shops" :key="shop.name">
     <h3 class="name">{{shop.name}}</h3>
    <p>{{shop.time}}</p>
-<div v-html="shop.location"></div>
-<img :src="require('@/assets/shops/'+ shop.pic)" alt="">
+<div class="map" v-html="shop.location"></div>
+<img class="pic" :src="require('@/assets/shops/'+ shop.pic)" alt="shop pic">
   </div>
 
    </div>
